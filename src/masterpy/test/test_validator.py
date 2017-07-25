2127220a674b8e706a8b6132da777fe388d7be35
David Jiménez <davigetto@gmail.com>
2017-07-25 23:21:52 +0200

    Completed the validation functionality with python

--- src/masterpy/test/test_validator.py
+++ src/masterpy/test/test_validator.py
@@ -11,11 +11,15 @@ class Colors(Enum):
 
 
 
-@pytest.mark.parametrize("solution,combination, expected_result", [
-    ([Colors.GREEN, Colors.GREEN], [Colors.GREEN, Colors.GREEN], Result(black=2, white=0)),
-    ([Colors.RED, Colors.RED], [Colors.RED, Colors.RED], Result(black=2, white=0)),
-    ([Colors.RED, Colors.GREEN], [Colors.RED, Colors.RED], Result(black=1, white=0)),
+@pytest.mark.parametrize(
+    "solution                       ,combination,                   expected_result", [
+    ([Colors.GREEN, Colors.GREEN],  [Colors.GREEN, Colors.GREEN],   Result(black=2, white=0)),
+    ([Colors.RED, Colors.RED],      [Colors.RED, Colors.RED],       Result(black=2, white=0)),
+    ([Colors.RED, Colors.GREEN],    [Colors.RED, Colors.RED],       Result(black=1, white=0)),
+    ([Colors.RED, Colors.GREEN],    [Colors.BLUE, Colors.BLUE],     Result(black=0, white=0)),
+    ([Colors.BLUE, Colors.RED],    [Colors.RED, Colors.BLUE],     Result(black=0, white=2)),
 ])
 def test_validate(solution, combination, expected_result):
     validator = Validator()
     assert validator.validate(solution, combination).black == expected_result.black
+    assert validator.validate(solution, combination).white == expected_result.white
--- src/masterpy/validator.py
+++ src/masterpy/validator.py
@@ -4,8 +4,20 @@ class Result:
         self.black = black
         self.white = white
 
-class Validator():
+class Validator:
+
+    def _to_dictionary(self, combination):
+        return dict((k, list(g)) for k, g in itertools.groupby(combination, lambda color: color))
+
     def validate(self, solution, combination):
         black = len(list(filter(lambda xy: xy[0] == xy[1], zip(solution, combination))))
 
-        return Result(black, 0)
+        grouped_by_color_solution = self._to_dictionary(solution)
+        grouped_by_color_combination = self._to_dictionary(combination)
+
+        white = 0
+        commoncolors = set(grouped_by_color_solution.keys()).intersection(set(grouped_by_color_combination.keys()))
+        for color in commoncolors:
+            white += min(len(grouped_by_color_solution[color]), len(grouped_by_color_combination[color]))
+
+        return Result(black, white - black)
